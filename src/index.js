8bfecd85a39b28b9ae4c12f0d1f746714ae70a0f
Kim Wijk <kim.wijk@gmail.com>
2016-07-07 00:33:13 +0200

    Add trailing commas

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ app.get('/upload', (req, res) => {
     Key: fileName,
     Expires: 60,
     ContentType: fileType,
-    ACL: 'public-read'
+    ACL: 'public-read',
   }
   s3.getSignedUrl('putObject', s3Params, (err, data) => {
     if(err){
@@ -46,7 +46,7 @@ app.get('/upload', (req, res) => {
     }
     const returnData = {
       signedRequest: data,
-      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
     }
     res.write(JSON.stringify(returnData))
     res.end()
